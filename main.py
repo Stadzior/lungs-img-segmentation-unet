1af77221a7a17458e41497f1dffa850fceafb259
Stadzior <Stadzior@gmail.com>
2019-05-19 10:41:57 +0200

    added training phase with measurements

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from model import *
-from data import *
+from data import *    
+import time
+import datetime
 
 TARGET_SIZE = (512, 512)
 EPOCH_COUNT = 2
@@ -19,3 +21,15 @@ AUG_PARAMETERS = dict(rotation_range=0.2,
 
 trainGenerator = CreateTrainGenerator(BATCH_SIZE, TARGET_SIZE, TRAIN_PATH, IMAGE_DIR,
                                       MASK_DIR, AUG_DIR, AUG_PARAMETERS)
+
+model = Unet((TARGET_SIZE[0], TARGET_SIZE[1], 1))
+
+#Executing training with timestamps and measurements
+startTmstmp = time.time()
+startDateTime = datetime.datetime.fromtimestamp(startTmstmp).strftime("%d-%m-%Y, %H:%M:%S")
+print("Training started at {%s}".format(startDateTime))
+model.fit_generator(trainGenerator, steps_per_epoch=SAMPLE_COUNT, epochs=EPOCH_COUNT)
+endTmstmp = time.time()
+endDateTime = datetime.datetime.fromtimestamp(endTmstmp).strftime("%d-%m-%Y, %H:%M:%S")
+print("Training ended at {%s} and lasted {%f}s".format(endDateTime, endDateTime-startDateTime))
+
