7e28102c9ba186feae5fadfe7e949ead9a0a3aed
Stadzior <Stadzior@gmail.com>
2019-05-19 12:55:55 +0200

    added timestamp executions and results saving template

--- data.py
+++ data.py
@@ -1,18 +1,22 @@
 from keras.preprocessing.image import ImageDataGenerator
 import numpy as np
+import skimage.transform as transform
+import glob
+import os
+import skimage.io as io
 
 def NormalizeData(img, mask):
     #normalization
     img = img / 255
     mask = mask /255
-    #treshholding
+    #tresholding
     mask[mask > 0.5] = 1
     mask[mask <= 0.5] = 0
-    return (img,mask)
+    return (img, mask)
 
-def CreateTrainGenerator(batch_size, target_size, train_path, image_dir, mask_dir, save_to_dir, aug_parameters,
+def CreateTrainGenerator(train_path, batch_size, target_size, image_dir, mask_dir, save_to_dir, aug_parameters,
                          image_color_mode = "grayscale", mask_color_mode = "grayscale", seed = 1):
-
+    #data augmentation
     image_generator = ImageDataGenerator(**aug_parameters)
     mask_generator = ImageDataGenerator(**aug_parameters)
     image_generator = image_generator.flow_from_directory(
@@ -39,3 +43,17 @@ def CreateTrainGenerator(batch_size, target_size, train_path, image_dir, mask_di
     for (img,mask) in train_generator:
         img,mask = NormalizeData(img, mask)
         yield (img,mask)
+        
+def CreateTestGenerator(test_path, target_size, img_limit = None):
+    os.chdir(test_path)
+    img_filenames = glob.glob("*.png")[:img_limit]
+    for img_filename in img_filenames:
+        img = io.imread(os.path.join(test_path,"{%s}.png".format(img_filename)), True)
+        img = img / 255
+        img = transform.resize(img, target_size)
+        # img = np.reshape(img, img.shape+(1,))
+        # img = np.reshape(img, (1,)+img.shape)
+        yield img
+
+def SaveResult(save_path, npy_file):
+    dummy = "dumm dumm dumm"
--- main.py
+++ main.py
@@ -8,9 +8,11 @@ EPOCH_COUNT = 2
 SAMPLE_COUNT = 500
 BATCH_SIZE = 1
 TRAIN_PATH = 'data/train'
+TEST_PATH = 'data/test'
 IMAGE_DIR = 'image'
 MASK_DIR = 'mask'
 AUG_DIR = 'aug'
+STEPS = 3
 AUG_PARAMETERS = dict(rotation_range=0.2,
                     width_shift_range=0.05,
                     height_shift_range=0.05,
@@ -19,17 +21,26 @@ AUG_PARAMETERS = dict(rotation_range=0.2,
                     horizontal_flip=True,
                     fill_mode='nearest')
 
-trainGenerator = CreateTrainGenerator(BATCH_SIZE, TARGET_SIZE, TRAIN_PATH, IMAGE_DIR,
-                                      MASK_DIR, AUG_DIR, AUG_PARAMETERS)
+def ExecuteWithTimestamps(func_desc, func_to_measure):   
+    start_tmstmp = time.time()
+    start_datetime = datetime.datetime.fromtimestamp(start_tmstmp).strftime("%d-%m-%Y, %H:%M:%S")
+    print("{%s} started at {%s}".format(func_desc, start_datetime))
+    result = func_to_measure()
+    end_tmstmp = time.time()
+    end_datetime = datetime.datetime.fromtimestamp(end_tmstmp).strftime("%d-%m-%Y, %H:%M:%S")
+    print("{%s} ended at {%s} and lasted {%f}s".format(func_desc, end_datetime, end_tmstmp-start_tmstmp))
+    return result
 
 model = Unet((TARGET_SIZE[0], TARGET_SIZE[1], 1))
 
 #Executing training with timestamps and measurements
-startTmstmp = time.time()
-startDateTime = datetime.datetime.fromtimestamp(startTmstmp).strftime("%d-%m-%Y, %H:%M:%S")
-print("Training started at {%s}".format(startDateTime))
-model.fit_generator(trainGenerator, steps_per_epoch=SAMPLE_COUNT, epochs=EPOCH_COUNT)
-endTmstmp = time.time()
-endDateTime = datetime.datetime.fromtimestamp(endTmstmp).strftime("%d-%m-%Y, %H:%M:%S")
-print("Training ended at {%s} and lasted {%f}s".format(endDateTime, endDateTime-startDateTime))
+trainGenerator = CreateTrainGenerator(TRAIN_PATH, BATCH_SIZE, TARGET_SIZE, IMAGE_DIR,
+                                      MASK_DIR, AUG_DIR, AUG_PARAMETERS)
+ExecuteWithTimestamps("Training", lambda _: model.fit_generator(trainGenerator, SAMPLE_COUNT, EPOCH_COUNT))
 
+#Executing testing with timestamps and measurements
+testGenerator = CreateTestGenerator(TEST_PATH, TARGET_SIZE)
+result = ExecuteWithTimestamps("Testing", lambda _: (model.predict_generator(testGenerator, STEPS, verbose = 1)))
+
+#Executing result saving with timestamps and measurements
+SaveResult("{%s}/{result_%s}".format(TEST_PATH, datetime.datetime.now.strftime("%d%m%Y_%H%M%S")), result)
