a06ed8796cb60fa786a7010414d67284d275c1aa
Stadzior <Stadzior@gmail.com>
2019-09-03 18:32:13 +0200

    added tensorboard instance

---
+++ tensorBoardInstance.py
@@ -0,0 +1,22 @@
+import logging
+from tensorboard import program
+from tensorboard import default
+import sys
+
+class TensorboardHelper:
+
+    def __init__(self, dir_path):
+        self.dir_path = dir_path
+
+    def run(self):
+        # Remove http messages
+        log = logging.getLogger('tensorflow').setLevel(logging.FATAL)
+        # Start tensorboard server
+        tb = program.TensorBoard(default.get_plugins(), default.get_assets_zip_provider())
+        tb.configure(argv=[None, '--logdir', self.dir_path])
+        url = tb.launch()
+        sys.stdout.write('TensorBoard at %s \n' % url)
+
+# Starting tensorboard server
+tensorboardServer = TensorboardHelper(".")
+tensorboardServer.run()
